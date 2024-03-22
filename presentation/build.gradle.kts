2320d32a867af283e9e0307586e3e87674a14c3f
HyeyeonPark <42951723+hyeyeon2371@users.noreply.github.com>
2024-03-22 14:51:18 +0900

    Update build.gradle.kts

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -46,7 +46,6 @@ android {
 
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":data"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
